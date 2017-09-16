ac0297c7719f44fe33508647f6b1e53a0b91c8f6
DarkYusuf13 <yusufdegermenci61616161@gmail.com>
2017-09-16 19:05:35 +0300

    Fix for MagmaCube

--- src/pocketmine/entity/monster/jumping/MagmaCube.php
+++ src/pocketmine/entity/monster/jumping/MagmaCube.php
@@ -36,8 +36,8 @@ class MagmaCube extends JumpingMonster{
         if($this->attackDelay > 10 && $this->distanceSquared($player) < 1){
             $this->attackDelay = 0;
             
-            $ev = new EntityDamageByEntityEvent($this, $player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $this->getDamage()));
-            $player->attack($ev);
+            $ev = new EntityDamageByEntityEvent($this, $player, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $this->getDamage());
+            $player->attack($ev->getFinalDamage(), $ev);
         }
     }
 
