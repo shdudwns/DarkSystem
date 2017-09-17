22cfc4df14d46a5a3248262ba24469833092e859
DarkYusuf13 <yusufdegermenci61616161@gmail.com>
2017-09-17 12:23:03 +0300

    Add new and update start utils

---
+++ Debug.cmd
@@ -0,0 +1,31 @@
+﻿@echo off
+TITLE DarkSystem
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist PocketMine-MP.phar (
+	set POCKETMINE_FILE=PocketMine-MP.phar
+) else (
+	if exist src\pocketmine\PocketMine.php (
+		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	) else (
+		echo "DarkSystem Bulunamadı!"
+		pause
+		exit 1
+	)
+)
+if exist bin\php\php_wxwidgets.dll (
+    %PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
+) else (
+    if exist bin\mintty.exe (
+        start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "DarkSystem Debug Modu" -i bin/darksystem.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+    ) else (
+        %PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+    )
+)
